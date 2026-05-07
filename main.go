8a4539acc5a0c3135c4ce8fe9c5c9fa4b25677d6
Adam Herro <adam.herro@comcast.net>
2026-05-07 19:13:59 -0400

    Synchronously checking a slice of URLs and outputting the result as a map and in the console in a table

--- main.go
+++ main.go
@@ -5,27 +5,38 @@ import (
 	"net/http"
 )
 
-// func URLChecker(a, b string) []bool {
-func URLChecker(a string) int {
-	aStatus := func() int {
-		res, err := http.Get(a)
-		if err != nil {
-			fmt.Println("Error")
+// Should take in a slice of strings and return the status code (int)
+// Loop through each string in the slice, run a goroutine for each loop using a channel.
+// Output the status code for each URL
+
+var URLsToCheck []string = []string{"https://adamherro.dev/", "https://vaudify.com/"}
+
+func URLChecker(urlList []string) map[string]bool {
+	urlResult := make(map[string]bool)
+
+	for _, url := range urlList {
+		status := checkURL(url)
+		if status != 200 {
+			fmt.Println("✗ -", url, "-", status)
+			urlResult[url] = false
+		} else {
+			fmt.Println("✓ -", url, "-", status, "(Ok)")
+			urlResult[url] = true
 		}
-		return res.StatusCode
 	}
 
-	aStatusCode := aStatus()
+	return urlResult
+}
 
-	if aStatusCode != 200 {
-		fmt.Println("✗ -", aStatusCode)
-	} else {
-		fmt.Println("✓ -", aStatusCode, "(Ok)")
+func checkURL(url string) int {
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return 0
 	}
-
-	return aStatusCode
+	return res.StatusCode
 }
 
 func main() {
-	URLChecker("https://adamherro.dev/")
+	URLChecker(URLsToCheck)
 }
--- main_test.go
+++ main_test.go
@@ -1,15 +1,24 @@
 package main
 
-// import (
-// 	"slices"
-// 	"testing"
-// )
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
 
-// func TestURLChecker(t *testing.T) {
-// 	got := URLChecker("")
-// 	want := []bool{true, true}
+func TestURLChecker(t *testing.T) {
+	newServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer newServer.Close()
 
-// 	if !slices.Equal(got, want) {
-// 		t.Errorf("Got %v want %v", got, want)
-// 	}
-// }
+	urls := []string{"https://adamherro.dev/", "https://vaudify.com/"}
+
+	got := URLChecker(urls)
+	want := map[string]bool{"https://adamherro.dev/": true, "https://vaudify.com/": true}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got %v want %v", got, want)
+	}
+}
